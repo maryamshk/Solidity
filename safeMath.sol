a9779ad96247209aae1e0fe4dab47d8e77f9af5f
Maryam Javed <maryamjavedshk@gmail.com>
2024-09-11 02:22:28 -0700

    Update safeMath.sol

--- safeMath.sol
+++ safeMath.sol
@@ -6,12 +6,12 @@ contract SafeMathTester{
 }
 
 function Add() public{
-    bigNumber=bigNumber+1;   //in older version(safe math library); its unchecked
+    bigNumber=bigNumber+1;   //in older version 0.6 and below(safe math library); its unchecked
     //  when we'll add one, since 255 is maximum, it will set value to 0
 }
 
 
-//in newer solidity version, it checks for overflow or underflow. the value will remain 255 evern after calling function
+//in newer solidity version 0.8 and above, it checks for overflow or underflow. the value will remain 255 even after calling the function
 //in newer version we can use {}
 
 //in newer version we can change it to 'uncheck' by using unchecked keyword
